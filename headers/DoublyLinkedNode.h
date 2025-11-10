d15fd30459f36810197869ce8d0b3e2847b2fd01
Marcus Marrio <marriomarcuslogin@gmail.com>
2025-11-10 14:01:42 +0800

    Added fix to DoublyLinkedNode

--- headers/DoublyLinkedNode.h
+++ headers/DoublyLinkedNode.h
@@ -12,8 +12,8 @@ public:
     static Node NIL;
 
     ~DoublyLinkedNode() = default;
-    DoublyLinkedNode(): next(NIL), previous(NIL) {}
-    explicit DoublyLinkedNode(T value): value(value), next(NIL), previous(NIL) {}
+    DoublyLinkedNode(): next(&NIL), previous(&NIL) {}
+    explicit DoublyLinkedNode(T value): value(value), next(&NIL), previous(&NIL) {}
 
     T& getValue() const {
         return value;
@@ -35,6 +35,44 @@ public:
     void setPrevious(Node* fPrevious) {
         this->previous = fPrevious;
     }
+
+    void prepend(Node* newNode) {
+        newNode->next = this;
+
+        if (this->previous != &NIL) {
+            newNode->previous = this->previous;
+            this->previous->next = newNode;
+        }
+
+        this->previous = newNode;
+    }
+
+    void append(Node* newNode) {
+        newNode->previous = this;
+
+        if (this->next != &NIL) {
+            newNode->next = this->next;
+            this->next->previous = newNode;
+        }
+
+        this->next = newNode;
+    }
+
+    void remove() {
+        if (this->previous != &NIL) {
+            this->previous->next = this->next;
+        }
+
+        if (this->next != &NIL) {
+            this->next->previous = this->previous;
+        }
+
+        this->next = &NIL;
+        this->previous = &NIL;
+
+        delete this;
+    }
+
 };
 
 template <class T>
