62b88e2edfdf703e871e851be5702b71212ab070
suliluz <marriomarcuslogin@gmail.com>
2025-11-26 01:54:38 +0800

    remove builds

--- out/build/x64-Debug/_deps/flac-build/src/libFLAC/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/marri/source/repos/ProgrammingProject_2025S2_101229262/out/build/x64-Debug/_deps/flac-src/src/libFLAC
-# Build directory: C:/Users/marri/source/repos/ProgrammingProject_2025S2_101229262/out/build/x64-Debug/_deps/flac-build/src/libFLAC
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
